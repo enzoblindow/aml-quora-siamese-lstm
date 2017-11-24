57b2123dc4c8eea85e12622637f53b5137368533
Enzo Blindow <enzo@blindow.eu>
2017-11-24 18:57:37 +0100

    import df function allows for unicode correction

--- helpers.py
+++ helpers.py
@@ -1,8 +1,21 @@
 import pandas as pd
 
 
-def create_train_data():
+def unicoder(str):
+    try:
+        return unicode(str)
+    except ValueError, TypeError:
+        return 'invalid'
+
+
+def create_train_data(unicoded=False):
     df_train = pd.read_csv('data/train_data.csv')
     df_train.drop(['is_duplicate'], axis=1, inplace=True)
     df_labels = pd.read_csv('data/train_labels.csv')
-    return df_train.merge(df_labels)
+    df = df_train.merge(df_labels)
+    if unicoded:
+        df.question1 = df.question1.apply(lambda x: unicoder(x))
+        df.question2 = df.question2.apply(lambda x: unicoder(x))
+        return df.loc[(df.question1 != 'invalid') | (df.question2 != 'invalid'), :]
+    else:
+        return df
